8ef3ce6d3123b58725bd9610b56ec7620ff09882
IaacobH <iaacobh@gmail.com>
2026-06-23 17:01:34 -0300

    the user decides if use FILETXT OR JSON

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,35 @@
 import Model.Product;
+import UI.InputUtils;
+
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-
+        Scanner input = new Scanner(System.in);
         Service.Inventory inventory = new Service.Inventory();
-        Repository.InventoryRepository repository = new Repository.JsonRepository();
+        Repository.InventoryRepository repository = null;
+
+        boolean validOption = false;
+        while(!validOption) {
+            System.out.println("\n===== STORAGE TYPE =====");
+            System.out.println("1 - JSON file");
+            System.out.println("2 - TXT file");
+            int option = InputUtils.getInt(input, "Choose storage option: ");
+
+            switch (option) {
+                case 1 -> {
+                    repository = new Repository.JsonRepository();
+                    validOption=true;
+                }
+                case 2 -> {
+                    repository = new Repository.FileInventoryRepository();
+                    validOption=true;
+                }
+                default -> System.out.println("Invalid option. Please choose 1 or 2.");
+            }
+        }
 
         List<Product> products = repository.read();
         inventory.addProducts(products);
