3418bf9d744364277b003dd6685bdef72c2683a4
IaacobH <iaacobh@gmail.com>
2026-06-23 22:29:34 -0300

    metodo choose repository bien hecho

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,39 +6,35 @@ import java.util.Scanner;
 
 public class Main {
 
-    private static Repository.InventoryRepository chooseRepository(Scanner input, Repository.InventoryRepository repository){
-        boolean validOption = false;
-        while(!validOption) {
+    private static Repository.InventoryRepository chooseRepository(Scanner input) {
+        while (true) {
             System.out.println("\n===== STORAGE TYPE =====");
             System.out.println("1 - JSON file");
             System.out.println("2 - TXT file");
+
             int option = InputUtils.getInt(input, "Choose storage option: ");
 
             switch (option) {
                 case 1 -> {
-                    repository = new Repository.JsonRepository();
-                    validOption=true;
+                    return new Repository.JsonRepository();
                 }
                 case 2 -> {
-                    repository = new Repository.FileInventoryRepository();
-                    validOption=true;
+                    return new Repository.FileInventoryRepository();
                 }
                 default -> System.out.println("Invalid option. Please choose 1 or 2.");
             }
         }
-        return repository;
     }
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
         Service.Inventory inventory = new Service.Inventory();
 
-        Repository.InventoryRepository repository = null;
-        repository = chooseRepository(input, repository);
+        Repository.InventoryRepository repository = chooseRepository(input);
 
         List<Product> products = repository.read();
         inventory.addProducts(products);
+
         UI.App.run(inventory);
 
         repository.save(inventory.getAllProducts());
